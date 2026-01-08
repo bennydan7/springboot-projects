65350d63cfc10434c9f04834d2a6497e307f9745
lonebhen <owusub243@gmail.com>
2026-01-08 06:14:21 +0000

    initial commit

---
+++ foodapi-backend/src/main/java/com/bennydan/foodapi_backend/FoodapiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bennydan.foodapi_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodapiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodapiBackendApplication.class, args);
+	}
+
+}
